4d1647fb10ec29d202d7620160e35ed84c4db188
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2021-04-21 13:35:03 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a2273982f03e8378b22a64119d670e7a'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8ccd411e-3e39-45d4-b23c-34133da50cc4'
+};
